1eafd5779b373057e9e57fc7b87d7247cc476651
Anton Kabachkov <anton.kabachkov@ibm.com>
2021-01-14 15:01:13 +0300

    generate/remove runtime directories and files

--- start.sh
+++ start.sh
@@ -2,6 +2,15 @@
 
 PWD=$(dirname $0)
 
+if [ ! -d runtime/humio/mounts/data ]; then mkdir -p runtime/humio/mounts/data; fi
+if [ ! -d runtime/humio/mounts/kafka-data ]; then mkdir -p runtime/humio/mounts/kafka-data; fi
+if [ ! -d runtime/humio/mounts/runtime-files ]; then mkdir -p runtime/humio/mounts/runtime-files; fi
+if [ ! -d runtime/filebeat/mounts ]; then
+  mkdir -p runtime/filebeat/mounts;
+  cp -R filebeat/sources runtime/filebeat/mounts/
+  touch runtime/filebeat/filebeat.yml
+fi
+
 docker-compose -f $PWD/docker-compose.yml build
-docker-compose -f $PWD/docker-compose.yml -d up
+docker-compose -f $PWD/docker-compose.yml up -d
 
--- stop.sh
+++ stop.sh
@@ -6,7 +6,4 @@ PWD=$(dirname $0)
 docker-compose -f $PWD/docker-compose.yml down
 docker-compose -f $PWD/docker-compose.yml rm
 
-rm -rf $PWD/runtime/filebeat/mounts/data/*
-rm -rf $PWD/runtime/humio/mounts/data/*
-rm -rf $PWD/runtime/humio/mounts/kafka-data/*
-rm -rf $PWD/runtime/humio/mounts/runtime_files/*
+rm -rf $PWD/runtime/*
